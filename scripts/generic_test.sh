3b9e3531742dab750da2dfac8986d18f4f16750a
root <root@danger36.cslcs.technion.ac.il>
2022-09-27 12:30:06 +0300

    Added generic test design

---
+++ scripts/generic_test.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+
+set -euo pipefail
+
+
+# Configurable parameters
+readonly TEST_NAME="$(basename -s .sh "$0")"
+readonly SETUP_NAME="danger36"
+readonly TESTS_ROOT="/homes/itaysnir/Kernel-networking-testsuite"
+readonly PERF="/homes/itaysnir/linux-stable/tools/perf/perf"
+readonly REPEAT_COUNT=3
+readonly REMOTE_PORT=8080
+readonly RAMP=5
+
+# No need to touch these
+readonly RESULTS_DIR="$TESTS_ROOT/Results"
+readonly DATE="$(date +%y_%m_%d-%H:%M:%S)"
+readonly OUT_DIR="$RESULTS_DIR/$TEST_NAME/$DATE"
+readonly COLLECT_CPU="$TESTS_ROOT/data_collectors/collect_net_cpu.sh"
+readonly COLLECT_SCRIPT="$TESTS_ROOT/data_collectors/collect.sh"
+readonly COLLECT_PCM_SCRIPT="$TESTS_ROOT/data_collectors/collect_pcm.sh"
+readonly MS_IN_SEC=1000
+
+
+log_info() {
+	local msg=$1
+	echo "[INFO]$msg"
+}
+
+
+log_error() {
+	local msg=$1
+	echo "[ERROR]$msg"
+}
+
+
+init_env() {
+	local i
+	for i in $(seq "$REPEAT_COUNT"); do
+		if [ ! -d "$OUT_DIR-$i" ]; then
+			mkdir -p "$OUT_DIR-$i"
+		fi
+	done
+	
+	# shellcheck source=/homes/itaysnir/Kernel-networking-testsuite/config/config_danger36.sh
+	source "$TESTS_ROOT/config/config_${SETUP_NAME}.sh"
+	$TESTS_ROOT/config/setup.sh "$SETUP_NAME"
+	log_info "Local configurations set"
+}
+
+
+kill_listening_process(){
+	local remote_port="$1"
+	local pids
+	
+	set +euo pipefail
+	pids=$(ssh "$loader1" pgrep -f $remote_port)
+	if [ -n "$pids" ]; then
+		ssh "$loader1" "sudo kill -9 $pids"	
+		sleep 0.5
+		log_info "Killed processes listening on remote port $remote_port"
+	fi
+	set -euo pipefail
+}
+
+run_nc() {
+	if [ -z "$(ssh $loader1 command -v nc)" ]; then 
+		log_error "No nc on the remote machine. Try: sudo apt install nc"
+		exit 1
+	fi
+
+	kill_listening_process "$REMOTE_PORT"
+
+	# TODO: check if running at backround is mandatory
+	ssh $loader1 "nc -l -s $dip1 -p $REMOTE_PORT &" &
+	log_info "Successfully launched nc server on $dip1:$REMOTE_PORT"
+	sleep 3
+}
+
+
+run_netserver() {
+        if [ -z "$(ssh $loader1 command -v netserver)" ]; then
+                log_error "No nc on the remote machine. Try: sudo apt install nc"
+                exit 1
+        fi
+
+	kill_listening_process "$REMOTE_PORT"
+
+        ssh $loader1 "sudo netserver -p $REMOTE_PORT"
+        log_info "Launched netserver on $dip1:$REMOTE_PORT"
+	sleep 3
+}
+
+
+run_test_multiple_times() {
+	local test_pid
+	local collect_cpu_pid
+	local collect_pid
+	local collect_pcm_pid
+	local test_output
+	local test_dir
+	
+	for i in $(seq "$REPEAT_COUNT"); do
+		test_dir="$OUT_DIR-$i"
+		test_output="$test_dir/test_raw.txt"
+		log_info "Running ${TEST_NAME}.. (iteration:$i)"
+			
+		run_nc
+
+		log_info "Sending packets.."
+		run_test "$i" &>> "$test_output" &
+		test_pid=$!
+
+		sleep "$RAMP"	
+		
+		log_info "Collecting data.."
+		$COLLECT_CPU "$if1" "$test_dir" &> /dev/null &
+		collect_cpu_pid=$!
+		
+		$COLLECT_SCRIPT &>> "$test_dir/result.txt" &
+		collect_pid=$!
+
+		$COLLECT_PCM_SCRIPT &>> "$test_dir/result_pcm.txt" &
+		collect_pcm_pid=$!
+
+		wait "$test_pid"
+		log_info "Done sending packets"
+
+		wait "$collect_cpu_pid"
+		wait "$collect_pid"
+		wait "$collect_pcm_pid"
+	done
+}
+
+
+generate_plots() {
+	local setup_csv="$TESTS_ROOT/Results/$TEST_NAME/setup.csv"
+	local filter_csv="$TESTS_ROOT/Results/$TEST_NAME/filter.csv"
+	local filter_csv_1="$filter_csv.1"
+
+	log_info "Test complete. Generating plots.."
+	$TESTS_ROOT/process/parse.py "$TESTS_ROOT/Results/$TEST_NAME" &> /dev/null
+
+	$TESTS_ROOT/process/filter.py "$setup_csv" > "$filter_csv"
+
+	head -n 1 "$filter_csv" > "$filter_csv_1"
+
+	log_info "Check results at:$OUT_DIR"
+}
+
--- tests/single_tcp_tx/single_tcp_tx.sh
+++ tests/single_tcp_tx/single_tcp_tx.sh
@@ -4,142 +4,27 @@
 set -euo pipefail
 
 
-# Configurable parameters
-readonly TEST_NAME="$(basename -s .sh "$0")"
-readonly SETUP_NAME="danger36"
-readonly TESTS_ROOT="/homes/itaysnir/Kernel-networking-testsuite"
-readonly PERF="/homes/itaysnir/linux-stable/tools/perf/perf"
-readonly REPEAT_COUNT=3
+readonly GENERIC_TEST="/homes/itaysnir/Kernel-networking-testsuite/scripts/generic_test.sh"
+source "$GENERIC_TEST"
+
 
 # Test Specific Config
-readonly IO_URING_BINARY="$TESTS_ROOT/tests/io_uring_tcp_tx/io_uring_tcp_tx"
-readonly REMOTE_IP="10.1.4.35"
-readonly REMOTE_PORT=8080
 readonly CHUNK_SIZE=32000
 readonly TIMEOUT=30
-readonly RAMP=5
-
-# No need to touch these
-readonly RESULTS_DIR="$TESTS_ROOT/Results"
-readonly DATE="$(date +%y_%m_%d-%H:%M:%S)"
-readonly OUT_DIR="$RESULTS_DIR/$TEST_NAME/$DATE"
-readonly COLLECT_CPU="$TESTS_ROOT/data_collectors/collect_net_cpu.sh"
-readonly COLLECT_SCRIPT="$TESTS_ROOT/data_collectors/collect.sh"
-readonly COLLECT_PCM_SCRIPT="$TESTS_ROOT/data_collectors/collect_pcm.sh"
-readonly MS_IN_SEC=1000
-
-
-log_info() {
-	local msg=$1
-	echo "[INFO]$msg"
-}
-
-
-log_error() {
-	local msg=$1
-	echo "[ERROR]$msg"
-}
-
-
-init_env() {
-	local i
-	for i in $(seq "$REPEAT_COUNT"); do
-		if [ ! -d "$OUT_DIR-$i" ]; then
-			mkdir -p "$OUT_DIR-$i"
-		fi
-	done
-	
-	# shellcheck source=/homes/itaysnir/Kernel-networking-testsuite/config/config_danger36.sh
-	source "$TESTS_ROOT/config/config_${SETUP_NAME}.sh"
-	$TESTS_ROOT/config/setup.sh "$SETUP_NAME"
-	log_info "Local configurations set"
-}
-
-
-run_netserver() {
-	local pids
-
-	if [ -z "$(ssh $loader1 command -v netserver)" ]; then 
-		log_error "No nc on the remote machine. Try: sudo apt install nc"
-		exit 1
-	fi
-
-	set +euo pipefail
-	pids=$(ssh "$loader1" pgrep -f $REMOTE_PORT)
-	if [ -n "$pids" ]; then
-		ssh "$loader1" "sudo kill -9 $pids"	
-		sleep 0.5
-		log_info "Killed processes listening on remote port $REMOTE_PORT"
-	fi
-	set -euo pipefail
-
-
-	ssh $loader1 "sudo netserver -p $REMOTE_PORT"
-	log_info "Launched netserver on $REMOTE_IP:$REMOTE_PORT"
-}
 
 
 run_test() {
 	local i="$1"
-	local cmdline="sudo netperf -H $dip1 -p $REMOTE_PORT -t TCP_STREAM -T 1,1 -l $TIMEOUT -- -m$CHUNK_SIZE -s16M"
+	local out_dir="$OUT_DIR-$i"
+	local cmdline="sudo $IO_URING_BINARY $dip1 $REMOTE_PORT $CHUNK_SIZE $TIMEOUT"
+
 	#shellcheck disable=SC2086
-	sudo -E "$PERF" stat -D $(( RAMP * MS_IN_SEC )) -a -C 0 -e duration_time,task-clock,cycles,instructions,cache-misses -x, -o "$OUT_DIR-$i/perf_stat.txt" --append ${cmdline} | tee -a "$OUT_DIR-$i/single_tcp.txt"
+	sudo -E "$PERF" stat -D $(( RAMP * MS_IN_SEC )) -a -C 0 -e duration_time,task-clock,cycles,instructions,cache-misses -x, -o "$out_dir/perf_stat.txt" --append ${cmdline} | tee -a "$OUT_DIR-$i/io_uring.txt"
 
-	dmesg | tail -n 90 >> "$OUT_DIR-$i/dmesg.txt"
-}
+#	sudo -E "$PERF" record -D $(( RAMP * MS_IN_SEC )) -a -C 0 -e duration_time,task-clock,cycles,instructions,cache-misses -d -T -o "$out_dir/perf.data" ${cmdline} | tee -a "$out_dir/io_uring.txt"
+#	sudo -E "$PERF" script -i "$out_dir/perf.data" > "$out_dir/perf.txt"
 
-
-run_test_multiple_times() {
-	local test_pid
-	local collect_cpu_pid
-	local collect_pid
-	local collect_pcm_pid
-	local test_output
-	local test_dir
-	
-	for i in $(seq "$REPEAT_COUNT"); do
-		test_dir="$OUT_DIR-$i"
-		test_output="$test_dir/test_raw.txt"
-		log_info "Running ${TEST_NAME}.. (iteration:$i)"
-			
-		run_netserver
-
-		run_test "$i" &>> "$test_output" &
-		test_pid=$!
-
-		sleep "$RAMP"	
-		log_info "Collecting data.."
-	
-		$COLLECT_CPU "$if1" "$test_dir" &> /dev/null &
-		collect_cpu_pid=$!
-		
-		$COLLECT_SCRIPT &>> "$test_dir/result.txt" &
-		collect_pid=$!
-
-		$COLLECT_PCM_SCRIPT &>> "$test_dir/result_pcm.txt" &
-		collect_pcm_pid=$!
-
-		wait "$test_pid"
-		wait "$collect_cpu_pid"
-		wait "$collect_pid"
-		wait "$collect_pcm_pid"
-	done
-}
-
-
-generate_plots() {
-	local setup_csv="$TESTS_ROOT/Results/$TEST_NAME/setup.csv"
-	local filter_csv="$TESTS_ROOT/Results/$TEST_NAME/filter.csv"
-	local filter_csv_1="$filter_csv.1"
-
-	log_info "Test complete. Generating plots.."
-	$TESTS_ROOT/process/parse.py "$TESTS_ROOT/Results/$TEST_NAME" &> /dev/null
-
-	$TESTS_ROOT/process/filter.py "$setup_csv" > "$filter_csv"
-
-	head -n 1 "$filter_csv" > "$filter_csv_1"
-
-	log_info "Check results at:$OUT_DIR"
+	dmesg | tail -n 30 >> "$out_dir/dmesg.txt"
 }
 
 
