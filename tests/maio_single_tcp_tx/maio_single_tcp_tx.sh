659c7c8c18839c2416bc290defd31f7123891501
root <root@danger36.cslcs.technion.ac.il>
2022-09-27 14:51:41 +0300

    automatic maio setup configuration

--- tests/maio_single_tcp_tx/maio_single_tcp_tx.sh
+++ tests/maio_single_tcp_tx/maio_single_tcp_tx.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 
 
 readonly GENERIC_TEST="/homes/itaysnir/Kernel-networking-testsuite/scripts/generic_test.sh"
-source "$GENERIC_TEST"
-
+# shellcheck source="/homes/itaysnir/Kernel-networking-testsuite/scripts/generic_test.sh"
+#source "$GENERIC_TEST"
 
 # Test Specific Config
 readonly MAIO_BINARY="$TESTS_ROOT/tests/maio_single_tcp_tx/maio_tcp_tx_client"
@@ -14,13 +14,24 @@ readonly TIMEOUT=30
 #readonly CHUNK_SIZE=32000
 
 
+init_maio() {
+	local huge_path="/mnt/huge"
+
+	sudo sh -c "echo 2048 > /proc/sys/vm/nr_hugepages"
+	sudo sh -c "echo 0 > /proc/sys/kernel/hung_task_timeout_secs"
+	sudo mkdir -p "$huge_path"
+	sudo mount -t hugetlbfs none "$huge_path"
+
+	log_info "MAIO - huge pages enabled"
+}
+
 run_test() {
 	local i="$1"
 	local out_dir="$OUT_DIR-$i"
 	local cmdline="sudo $MAIO_BINARY $TIMEOUT"
 
 	#shellcheck disable=SC2086
-	sudo -E "$PERF" stat -D $(( RAMP * MS_IN_SEC )) -a -C 0 -e duration_time,task-clock,cycles,instructions,cache-misses -x, -o "$out_dir/perf_stat.txt" --append ${cmdline} | tee -a "$OUT_DIR-$i/maio.txt"
+	sudo -E "$PERF" stat -D $(( RAMP * MS_IN_SEC )) -a -C 0 -e duration_time,task-clock,cycles,instructions,cache-misses -x, -o "$out_dir/perf_stat.txt" --append ${cmdline} | tee -a "$out_dir/maio.txt"
 
 #	sudo -E "$PERF" record -D $(( RAMP * MS_IN_SEC )) -a -C 0 -e duration_time,task-clock,cycles,instructions,cache-misses -d -T -o "$out_dir/perf.data" ${cmdline} | tee -a "$out_dir/io_uring.txt"
 #	sudo -E "$PERF" script -i "$out_dir/perf.data" > "$out_dir/perf.txt"
@@ -29,6 +40,7 @@ run_test() {
 
 main() {
 	init_env
+	init_maio
 	run_test_multiple_times "nc"
 	generate_plots	
 }
