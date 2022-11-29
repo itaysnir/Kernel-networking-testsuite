f43f70d72c2261f603b426f67f8296ce69608177
itaysnir <itaysnir3687@gmail.com>
2022-11-29 12:16:21 +0200

    added send ring buffer

--- tests/manual_single_tcp_tx/manual_tcp.c
+++ tests/manual_single_tcp_tx/manual_tcp.c
@@ -5,6 +5,7 @@
     #include <netdb.h> 
     #include <sys/types.h> 
     #include <netinet/in.h> 
+    #include <netinet/tcp.h>
     #include <sys/socket.h> 
     #include <unistd.h>
     #include <arpa/inet.h>
@@ -13,9 +14,36 @@
     #define PORT 8080    /* the port client will be connecting to */
     #define CHUNK 16384
 
+struct ring_elt {
+  struct ring_elt *next;  /* next element in the ring */
+  char *buffer_base;      /* in case we have to free it at somepoint */
+  char *buffer_ptr;       /* the aligned and offset pointer */
+  void *completion_ptr;   /* a pointer to information for async completion */
+  /* these are for sendfile calls and at some point we should consider
+     using a union but it isn't really all that much extra space */
+  struct iovec *hdtrl;            /* a pointer to a header/trailer
+                                     that we do not initially use and
+                                     so should be set to NULL when the
+                                     ring is setup. */
+  off_t offset;                   /* the offset from the beginning of
+                                     the file for this send */
+  size_t length;                  /* the number of bytes to send -
+                                     this is redundant with the
+                                     send_size variable but I decided
+                                     to include it anyway */
+  int fildes;                     /* the file descriptor of the source
+                                     file */
+  int flags;                      /* the flags to pass to sendfile() -
+                                     presently unused and should be
+                                     set to zero when the ring is
+                                     setup. */
+};
+
+
     int main(int argc, char *argv[])
     {
         int sockfd;  
+	int sent_len = 0;
         struct hostent *he;
         struct sockaddr_in their_addr; /* connector's address information */
 
@@ -34,17 +62,51 @@
             exit(1);
         }
 
+	// TODO: delete this
 	void *big_buf = malloc(CHUNK);
 	memset(big_buf, 'A', CHUNK);
 
+	struct ring_elt *prev_link = NULL;
+       	struct ring_elt *temp_link = NULL;
+	struct ring_elt *first_link = NULL;
+	int alignment = 16;
+
+	for (int i = 1 ; i <= 2048 ; i++)
+	{
+		temp_link = (struct ring_elt *)malloc(sizeof(struct ring_elt));
+		temp_link->completion_ptr = NULL;
+		if (i == 1){
+			first_link = temp_link;
+		}
+		
+		temp_link->buffer_base = (char *)malloc(CHUNK + alignment); 
+		memset(temp_link->buffer_base, 0, CHUNK + alignment);
+
+		temp_link->buffer_ptr = (char *)(( (long)temp_link->buffer_base + (long)alignment -1 ) & ~((long)alignment - 1));
+		
+		char *bufptr = temp_link->buffer_ptr;
+		memset(bufptr, 'A', CHUNK);	
+		
+		temp_link->next = prev_link;
+		prev_link = temp_link;	
+	}	
+	
+	if (first_link)
+	{
+		// make this a circular list
+		first_link->next = temp_link;
+	}
+		
+
+
  	size_t sk_buf_size = 16777216;
-	int reuse_addr = 1;
+	int one = 1;
 
-	setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, (void *)&sk_buf_size, sizeof(int));
-	setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, (void *)&sk_buf_size, sizeof(int));
-	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (void *)&reuse_addr, sizeof(int));
-	setsockopt(sockfd, SOL_IP, IP_RECVERR, (void *)&reuse_addr, sizeof(int)); // just enable
-
+	setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, (void *)&sk_buf_size, sizeof(one));
+	setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, (void *)&sk_buf_size, sizeof(one));
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (void *)&one, sizeof(one));
+	setsockopt(sockfd, SOL_IP, IP_RECVERR, (void *)&one, sizeof(one)); // just enable
+	// setsockopt(sockfd, SOL_TCP, TCP_NODELAY, (void *)&one, sizeof(one));
 	
 	struct sockaddr_in local_addr;
 	memset(&local_addr, 0, sizeof(struct sockaddr_in));
@@ -64,12 +126,22 @@
             perror("connect");
             exit(1);
         }
+
+	temp_link = first_link;
+
 	while (1) {
-		if (sendto(sockfd, big_buf, CHUNK, 0, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1){
-                      perror("send");
-		      exit (1);
+//		if (sendto(sockfd, big_buf, CHUNK, 0, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1){
+//                     perror("send");
+//		      exit (1);
+//		}
+	
+		sent_len = send(sockfd, temp_link->buffer_ptr, CHUNK, 0);
+		if (sent_len != CHUNK)
+		{
+			printf("Invalid Sent length:%d\n", sent_len);
+			exit(1);
 		}
-		write(1, "After the send function \n", 30);
+		temp_link = temp_link->next;
 	}
 
         close(sockfd);
